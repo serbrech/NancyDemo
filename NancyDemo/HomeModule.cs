bac27d1f7bf1cadda2603122ae9d639c01e6dd13
Stéphane <stephane.erbrech@gmail.com>
2014-04-02 18:50:50 +0000

    init

---
+++ NancyDemo/HomeModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NancyDemo
+{
+    public class HomeModule : Nancy.NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => View["index.html"];
+        }
+    }
+}
